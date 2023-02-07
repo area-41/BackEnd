87303838c0b5f8848e3be93047ff2cf9d224233b
Área 41 - Laboratório de Pesquisa e Análise <87396846+area-41@users.noreply.github.com>
2023-02-07 10:13:28 -0300

    Update IPhoneFactory.java

--- ProgramacaoOreintadaObjetos_02/IPHONE/IPhoneFactory.java
+++ ProgramacaoOreintadaObjetos_02/IPHONE/IPhoneFactory.java
@@ -1,43 +1,14 @@
 package br.com.americanas.polotech.ProOri_02.IPhone;
 
-import java.math.BigDecimal;
-
-public class IPhoneFactory implements IPhone{
-
-        private String modelo;
-        private Integer tela;
-        private Integer memoria;
-        private Integer camera;
-        private BigDecimal preco;
-
-        public IPhoneFactory(String modelo, Integer tela, Integer memoria, Integer camera, BigDecimal preco) {
-            this.modelo = modelo;
-            this.tela = tela;
-            this.memoria = memoria;
-            this.camera = camera;
-            this.preco = preco;
+public class IPhoneFactory {
+    public IPhone cadastrarIPhone(TypeIPhoneEnum typeIPhoneEnum) {
+        if (typeIPhoneEnum.equals(TypeIPhoneEnum.IPHONE_9)) {
+            return new IPhone9();
+        } else if (typeIPhoneEnum.equals(TypeIPhoneEnum.IPHONE_X)) {
+            return new IPhone_X();
+        } else if (typeIPhoneEnum.equals(TypeIPhoneEnum.IPHONE_13_MINI)) {
+            return new IPhone_13_Mini();
         }
-
-    @Override
-    public String toString() {
-        return "I-Phone{ " +
-                "modelo: " + modelo +
-                "| tela: " + tela +
-                "| memoria: " + memoria +
-                "| camera: " + camera +
-                "| preco: " + preco +
-                '}';
+        return null;
     }
-
-    public IPhoneFactory() {
-    }
-
-    @Override
-        public void exibirDetalhes() {
-            IPhoneFactory iPhoneFactory = new IPhoneFactory();
-            for (TypeIPhoneEnum typeIPhoneEnum : TypeIPhoneEnum.values()) {
-                System.out.println(typeIPhoneEnum.toString());
-            }
-
-        }
 }
